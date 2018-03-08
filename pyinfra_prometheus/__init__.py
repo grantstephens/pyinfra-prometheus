f47c0b1988412acf971e5af0f8932bec4ad1c19c
Grant Stephens <grant@stephens.co.za>
2018-03-08 09:36:24 +0000

    Add node_exporter only deploy

--- pyinfra_prometheus/__init__.py
+++ pyinfra_prometheus/__init__.py
@@ -4,13 +4,19 @@
 
 from pyinfra.api import deploy
 
-from .prometheus import configure_prometheus, install_prometheus
 from .node_exporter import configure_node_exporter, install_node_exporter
+from .prometheus import configure_prometheus, install_prometheus
 
 
-@deploy('Deploy prometheus')
+@deploy('Deploy prometheus (Includes node exporter)')
 def deploy_prometheus(state, host, enable_service=True):
     install_prometheus(state, host)
     configure_prometheus(state, host, enable_service=enable_service)
+    install_node_exporter(state, host)
+    configure_node_exporter(state, host, enable_service=enable_service)
+
+
+@deploy('Deploy the node exporter only')
+def deploy_node_exporter(state, host, enable_service=True):
     install_node_exporter(state, host)
     configure_node_exporter(state, host, enable_service=enable_service)
--- setup.py
+++ setup.py
@@ -5,8 +5,10 @@ if __name__ == '__main__':
     setup(
         version='0.1',
         name='pyinfra-prometheus',
+        url='https://github.com/grantstephens/pyinfra-prometheus',
         description='Install & bootstrap prometheus clusters with pyinfra.',
         packages=find_packages(),
         install_requires=('pyinfra>=0.5'),
         include_package_data=True,
+        license='MIT',
     )
